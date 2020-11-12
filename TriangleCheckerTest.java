c6305ca4f12eeafac03c7b877b4c02faa48a73fc
Maxleton <73335192+Maxleton@users.noreply.github.com>
2020-11-12 19:34:33 +0100

    Add files via upload

--- TriangleCheckerTest.java
+++ TriangleCheckerTest.java
@@ -7,34 +7,46 @@ public class TriangleCheckerTest {
     public void testValidTriangle() {
 
         // Arrange
-        float a = -2;
-        float b = -4;
-        float c = -4;
+        float a = 3;
+        float b = 3;
+        float c = 0;
 
         // Act
         TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
 
-        // Assert
-        /*assertEquals(TriangleChecker.TriangleType.NORMAL, type);*/
-        assertEquals(TriangleChecker.TriangleType.NONE, type);
-
     }
-     @Test
 
+    @Test 
+    public void checkNormal(){
+      assertEquals(TriangleChecker.TriangleType.NORMAL, TriangleChecker.checkTriangle( 1, 2, 3 ));
+    }
+
+    @Test 
+    public void checkNone(){
+      assertEquals(TriangleChecker.TriangleType.NONE, TriangleChecker.checkTriangle( 0, 1, 2 ));
+    }
+
+    @Test 
+    public void checkIsosceles(){
+      assertEquals(TriangleChecker.TriangleType.ISOSCELES, TriangleChecker.checkTriangle( 1, 2, 2 ));
+    }
+
+    @Test 
+    public void checkEquilateral(){
+      assertEquals(TriangleChecker.TriangleType.EQUILATERAL, TriangleChecker.checkTriangle( 1, 1, 1 ));
+    }
+
+    @Test
     public void testNoTriangleThreeNegativeSides() {
-
         assertEquals( TriangleChecker.TriangleType.NONE, TriangleChecker.checkTriangle( -1f, -1f, -1f ) );
-
     }
 
 
 
     @Test
-
     public void testNoTriangleTwoNegativeSides() {
 
         assertEquals( TriangleChecker.TriangleType.NONE, TriangleChecker.checkTriangle( -1f, -1f, 1f ) );
-
     }
 
 
